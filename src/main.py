d1f6d3ec3a1950a7eedc7912c165ea1ac4fc1cad
dimka90 <dimkayilrit@gmail.com>
2025-11-10 14:40:12 +0100

    setup: setup  project structure

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+from pydantic import BaseModel, Field
+from dataclasses import dataclass
+from typing import List
+
+class Interpreter:
+    def __init__(self):
+        self.stack: List[int] = []
