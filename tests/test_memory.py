76f0d0034096710121bbe8682b7ee2d47a15ce84
dimka90 <dimkayilrit@gmail.com>
2025-11-22 10:19:54 +0100

    test:add test for store8

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -6,10 +6,16 @@ import pytest
 def memory():
     " Returns a fresh memomry instance"
     return Memory()
-def test_data_len(memory):
+
+def test_empty_memory(memory):
     assert len(memory.data) == 0
 
 def test_store(memory):
     memory.store(0, 10)
     assert len(memory.data) == WORD_SIZE
     assert memory.load(0) == 10
+
+@pytest.mark.parametrize("offset, value", [(0,12), (12, 30), (32,0xFF)])
+def test_store8_various_values(memory, offset, value):
+    memory.store8(offset, value)
+    assert memory.data[offset] == value
